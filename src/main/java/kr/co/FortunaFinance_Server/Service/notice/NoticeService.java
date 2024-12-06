f1f820aa6d7c3f387baef3860e2f4a03c1b297db
ASH <soxyxs4996@gmail.com>
2024-12-06 15:46:00 +0900

    Feat_2024.12.06_Save_9_15:40 NoticeServiceImpl.java NoticeService.java Notice Service 수정

--- src/main/java/kr/co/FortunaFinance_Server/Service/notice/NoticeService.java
+++ src/main/java/kr/co/FortunaFinance_Server/Service/notice/NoticeService.java
@@ -1,8 +1,12 @@
 package kr.co.FortunaFinance_Server.Service.notice;
 
+import kr.co.FortunaFinance_Server.DTO.notice.NoticeListDTO;
+import kr.co.FortunaFinance_Server.DTO.notice.NoticeListReq;
 import kr.co.FortunaFinance_Server.DTO.notice.NoticeadditionReq;
 
 public interface NoticeService {
 
     int Notice_addition(NoticeadditionReq boardadditionReq);
+
+    NoticeListDTO Notice_list(NoticeListReq boardlistReq);
 }
--- src/main/java/kr/co/FortunaFinance_Server/Service/notice/NoticeServiceImpl.java
+++ src/main/java/kr/co/FortunaFinance_Server/Service/notice/NoticeServiceImpl.java
@@ -2,6 +2,8 @@ package kr.co.FortunaFinance_Server.Service.notice;
 
 import kr.co.FortunaFinance_Server.DAO.notice.NoticeDAO;
 import kr.co.FortunaFinance_Server.DAO.user.LoginRegisterDAO;
+import kr.co.FortunaFinance_Server.DTO.notice.NoticeListDTO;
+import kr.co.FortunaFinance_Server.DTO.notice.NoticeListReq;
 import kr.co.FortunaFinance_Server.DTO.notice.NoticeadditionReq;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -16,5 +18,11 @@ public class NoticeServiceImpl implements NoticeService {
     public int Notice_addition(NoticeadditionReq boardadditionReq) {
 
         return noticeDAO.Notice_addition(boardadditionReq);
+    }
+
+    @Override
+    public NoticeListDTO Notice_list(NoticeListReq boardlistReq) {
+
+        return null;
     }
 }
