b01642c627fb8647a9f91a63ae256f6cfcf55304
ASH <soxyxs4996@gmail.com>
2024-12-06 11:29:48 +0900

    Feat_2024.12.06_Save_4_11:00 NoticeService.java NoticeServiceImpl.java 작성

---
+++ src/main/java/kr/co/FortunaFinance_Server/Service/notice/NoticeService.java
@@ -0,0 +1,8 @@
+package kr.co.FortunaFinance_Server.Service.notice;
+
+import kr.co.FortunaFinance_Server.DTO.notice.NoticeadditionReq;
+
+public interface NoticeService {
+
+    int Notice_addition(NoticeadditionReq boardadditionReq);
+}
---
+++ src/main/java/kr/co/FortunaFinance_Server/Service/notice/NoticeServiceImpl.java
@@ -0,0 +1,15 @@
+package kr.co.FortunaFinance_Server.Service.notice;
+
+import kr.co.FortunaFinance_Server.DTO.notice.NoticeadditionReq;
+import org.springframework.stereotype.Service;
+
+@Service
+public class NoticeServiceImpl implements NoticeService {
+
+
+    @Override
+    public int Notice_addition(NoticeadditionReq boardadditionReq) {
+
+        return 0;
+    }
+}
